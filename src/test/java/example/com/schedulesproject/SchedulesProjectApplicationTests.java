0194223e9a367c6d24d6a8200902ad9e9ed1cdcb
Gi0000 <102645896+Gi0000@users.noreply.github.com>
2025-05-07 17:28:40 +0900

    프로젝트 생성

---
+++ src/test/java/example/com/schedulesproject/SchedulesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package example.com.schedulesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulesProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
